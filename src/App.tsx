4ce4e9e8061bb9b37d79f729aedb778fdb6c596b
DanArtt <xdogman5@gmail.com>
2022-11-25 12:35:45 -0300

    criando o login e estilizando

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './paginas/home/home';
+import {Grid} from '@material-ui/core'
+import Navbar from './componentes/estaticos/navbar/navbar';
+import Footer from './componentes/estaticos/footer/footer';
+import Login from './paginas/login/login';
 
 
 
 function App() {
   return (
     <>
-      <Home />
-      
+      <Router>
+        <Navbar/> 
+        <Routes>
+          <Route path='/home' element={<Home/>}> </Route>
+
+          <Route path='/login' element={<Login/>}> </Route>
+
+        </Routes>
+        <Footer/>
+      </Router>
     </>
   );
 }
