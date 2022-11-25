6b71abcbbce70356349dc932e29c7d56e411d7e6
DanArtt <xdogman5@gmail.com>
2022-11-25 12:33:50 -0300

    criando o login e estilizando

---
+++ src/paginas/login/login.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid, Typography, TextField, Button } from '@material-ui/core';
+import {Box} from '@mui/material';
+import { Link } from 'react-router-dom';
+import './login.css';
+
+function Login() {
+
+    return (
+        <Grid container direction='row' justifyContent='center' alignItems='center' style={{background: "#222222"}}>
+            <Grid alignItems='center' xs={6}>
+                <Box paddingX={20}>
+                    <form>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1' style={{color: "white"}} >Entrar</Typography>
+                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+                        <Box marginTop={2} textAlign='center' style={{color: "white"}}>
+                                <Button type='submit' variant='contained' color='primary' style={{background: "black"}}>
+                                    Logar
+                                </Button>
+                        </Box>
+                    </form>
+                    <Box display='flex' justifyContent='center' marginTop={2} >
+                        <Box marginRight={1}>
+                            <Typography variant='subtitle1' gutterBottom align='center' style={{color: "white"}}>Não tem uma conta?</Typography>
+                        </Box>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='textos1' style={{color: "white"}}>Cadastre-se</Typography>
+                    </Box>
+                </Box>
+            </Grid>
+            <Grid xs={6} className='imagem'>
+
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Login;
